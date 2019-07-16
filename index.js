adede489a79bdf4217c4c9ad845f404cb043921b
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-07-16 17:02:14 -0300

    configurando acesso a api

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
 
 const assistant = new AssistantV1({
-  version: '{version}',
-  iam_apikey: '{apikey}',
-  url: '{url}'
+  version: '16-07-19',
+  iam_apikey: 'ZBr_n5VroO7pL2ywA9SDTxUcqNpyJsAXp2Q-Jssh-lBn',
+  url: 'https://gateway.watsonplatform.net/assistant/api'
 });
+
+service.message({
+    workspace_id: '7386122e-5a23-4094-a574-0ac27a7c7244',
+    input: {'text': 'Quanto tempo demora pro meu pedido ser entregue?'}
+    })
+    .then(res => {
+      console.log(JSON.stringify(res, null, 2));
+    })
+    .catch(err => {
+      console.log(err)
+    });
+    
