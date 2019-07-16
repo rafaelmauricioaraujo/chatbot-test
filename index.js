566f2825b25faaab4014549b1835e866638305a2
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-07-16 17:04:56 -0300

    text interactions

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const AssistantV1 = require('ibm-watson/assistant/v1');
 
 const assistant = new AssistantV1({
-  version: '16-07-19',
+  version: '2019-07-16',
   iam_apikey: 'ZBr_n5VroO7pL2ywA9SDTxUcqNpyJsAXp2Q-Jssh-lBn',
   url: 'https://gateway.watsonplatform.net/assistant/api'
 });
 
-service.message({
+assistant.message({
     workspace_id: '7386122e-5a23-4094-a574-0ac27a7c7244',
     input: {'text': 'Quanto tempo demora pro meu pedido ser entregue?'}
     })
@@ -16,4 +16,3 @@ service.message({
     .catch(err => {
       console.log(err)
     });
-    
