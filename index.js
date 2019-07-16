2fc11eb07fd9799a7cad94c0a73985559caa03fe
rafaelmauricioaraujo <rafael.mauricio.araujo@gmail.com>
2019-07-16 18:25:51 -0300

    testando logs

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ const assistant = new AssistantV1({
   url: 'https://gateway.watsonplatform.net/assistant/api'
 });
 
+const params = {
+    workspace_id: '7386122e-5a23-4094-a574-0ac27a7c7244',
+}
+
 assistant.message({
     workspace_id: '7386122e-5a23-4094-a574-0ac27a7c7244',
     input: {'text': 'Quanto tempo demora pro meu pedido ser entregue?'}
@@ -16,3 +20,11 @@ assistant.message({
     .catch(err => {
       console.log(err)
     });
+
+assistant.listLogs(params)
+    .then(res => {
+      console.log(JSON.stringify(res, null, 2));
+    })
+    .catch(err => {
+      console.log(err)
+    });    
